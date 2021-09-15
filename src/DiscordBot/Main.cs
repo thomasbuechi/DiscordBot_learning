f9e21fd43a6e034b5e90a51f9fb17cb3955b6eee
buechth1 <t.buechi@gmx.ch>
2021-09-15 18:37:20 +0200

    added websocket login for bot

--- src/DiscordBot/Main.cs
+++ src/DiscordBot/Main.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace DiscordBot_learning
 {
-    internal class Program
+    public class Program
     {
+        private DiscordSocketClient client;
+        
         public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello");
+            const string token = "ODg3NzEwODk5MTc0MDA2Nzg0.YUIHMw._0iXDhLhGQZj5DnEcDEKzt5eVt8";
+            client = new DiscordSocketClient();
+            client.Log += Log;
+            
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
+
 }
